02e916a267d8d4e6c43e13879e8fd3f539fe6c8a
lookpearsw <supajeew@gmail.com>
2019-10-16 14:49:14 +0700

    navigation4

--- app/src/main/java/com/gmail/supajeew/project02/HomeFragment.kt
+++ app/src/main/java/com/gmail/supajeew/project02/HomeFragment.kt
@@ -2,12 +2,11 @@ package com.gmail.supajeew.project02
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.gmail.supajeew.project02.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -25,9 +24,18 @@ class HomeFragment : Fragment() {
         binding.greetingButton.setOnClickListener { view : View ->
             view.findNavController().navigate(R.id.action_homeFragment_to_greeting_Fragment)
         }
-
+        setHasOptionsMenu(true)
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.option_menu, menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+            view!!.findNavController())
+                || super.onOptionsItemSelected(item)
+    }
 }
--- app/src/main/java/com/gmail/supajeew/project02/MainActivity.kt
+++ app/src/main/java/com/gmail/supajeew/project02/MainActivity.kt
@@ -3,6 +3,8 @@ package com.gmail.supajeew.project02
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.gmail.supajeew.project02.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
