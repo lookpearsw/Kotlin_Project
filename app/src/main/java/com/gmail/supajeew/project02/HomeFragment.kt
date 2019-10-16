de436d89233d248bdd6338fdc6fe64e51acd3fab
lookpearsw <supajeew@gmail.com>
2019-10-16 09:57:51 +0700

    first commit

--- app/src/main/java/com/gmail/supajeew/project02/HomeFragment.kt
+++ app/src/main/java/com/gmail/supajeew/project02/HomeFragment.kt
@@ -6,18 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.gmail.supajeew.project02.databinding.FragmentHomeBinding
 
-/**
- * A simple [Fragment] subclass.
- */
 class HomeFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater,
+            R.layout.fragment_home,container,false)
+        return binding.root
     }
 
 
